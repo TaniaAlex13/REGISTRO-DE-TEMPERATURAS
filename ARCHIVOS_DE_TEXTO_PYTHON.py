d371bd33db1bd01f65cbfe2fbb55157bfeba4c50
TaniaAlex13 <ta.toapantac@uea.edu.ec>
2025-10-04 13:27:35 -0500

    "Ejercicio de Archivos de texto en Python"

--- ARCHIVOS_DE_TEXTO_PYTHON.py
+++ ARCHIVOS_DE_TEXTO_PYTHON.py
@@ -1,26 +1,26 @@
 #TRABAJO DE ARCHIVOS DE TEXTO EN PYTHON
 # Abrimos el archivo en modo escritura
-file = open("my_notes.txt", "w")
+archivo = open("my_notes.txt", "w")
 
 # Escribimos tres líneas de notas personales usando write()
 
-file.write("Primera nota: En programacion estamos aprendiendo a crear archivos de texto en Python.\n")
-file.write("Segunda nota: El practicar con ejemplos nos esta sirviendo de mucho.\n")
-file.write("Tercera nota: Gracias a eso aprenderemos mas sobre el tema y lograremos presentar bien la tarea.\n")
+archivo.write("Primera nota: En programacion estamos aprendiendo a crear archivos de texto en Python.\n")
+archivo.write("Segunda nota: El practicar con ejemplos nos esta sirviendo de mucho.\n")
+archivo.write("Tercera nota: Gracias a eso aprenderemos mas sobre el tema y lograremos presentar bien la tarea.\n")
 
 # Cerramos el archivo después de escribir
-file.close()
+archivo.close()
 
 # Lectura del archivo de texto
 
 # Abrimos el archivo en modo lectura
-file = open("my_notes.txt", "r")
+archivo = open("my_notes.txt", "r")
 
 # Leemos cada línea con readline()
-linea = file.readline()
+linea = archivo.readline()
 while linea:
     print(linea.strip())
-    linea = file.readline()
+    linea = archivo.readline()
 
 # Cerramos el archivo después de leer
-file.close()
+archivo.close()
